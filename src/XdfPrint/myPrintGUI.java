32cecc2cbc1c73c5581e40eec843d8b70e6ada15
Alex Qu <ghostqu0@gmail.com>
2016-11-04 16:01:25 -0400

    .

--- src/XdfPrint/myPrintGUI.java
+++ src/XdfPrint/myPrintGUI.java
@@ -1,25 +1,63 @@
 package XdfPrint;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
 import javax.swing.*;
 
 public class myPrintGUI {
+	// data member
+	private PrinterJob pj = PrinterJob.getPrinterJob();
+	private MenuItem convertPdg2Ps = new MenuItem("Convert");
+	private MenuItem printSetup = new MenuItem("Print Setup");
+	
+	// method part
 	public myPrintGUI(){}
+	
+	//TODO: maybe we should add a file choose window; 
+	
+	/*@author Alex Qu
+	 * NOTIC!!! in this method you must deploy printMenuItem() to add action listener to menu item
+	 * */
 	public void GUIInitial(){}
+	
+	/*@author Alex Qu
+	 * this method is used to set the action of menu item
+	 * */
 	public void printMenuItem(){
+		convertPdg2Ps.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		
+		printSetup.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
-	/*author Alex Qu
+	
+	/*@author Alex Qu
 	 * this method is used to create a printer dialog
 	 * in the dialog the information of page format should be delivery to service class
 	 * */
 	public void myPrintDialog(){
-		PrinterJob pj = PrinterJob.getPrinterJob();
+		//PrinterJob pj = PrinterJob.getPrinterJob();
 		if(pj.printDialog()){
 			try{pj.print();}
 			catch(PrinterException exc){
+				//TODO get a warning window
 				System.out.println(exc);
 			}
 		}
