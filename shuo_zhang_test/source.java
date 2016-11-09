e3b7515b8465f25450b5ad9844fe45e7c53dc78d
zs65927637 <szhang49@stevens.edu>
2016-11-09 17:33:43 -0500

    Create source.java

---
+++ shuo_zhang_test/source.java
@@ -0,0 +1,12 @@
+Class xprint{
+/*transfer a pdf file into postscript format*/
+Public void transfer(){}
+/*open window and get the path of file*/
+Public void filelocation(){}
+/*get all available printer information*/
+Public void printerlist(){}
+/*read in the file from the file location*/
+Public void fileopen(){}
+/*output the postscript format file to the printer*/
+Public void printfile(){}
+}
